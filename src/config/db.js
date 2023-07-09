62679f9056bfc2d4358d133ed5ffc7707ea19a5d
zukhriddin1 <zukhriddingiyosiddinov@gmail.com>
2023-07-09 17:30:45 +0500

    canhe1

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,12 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize("countries", "postgres", "1234zzzz", {
-  host: "localhost",
-  dialect: "postgres",
   port: 5432,
+  user: "postgres",
+  password: "1234zzzz",
+  host: "localhost",
+  database: "countries",
+  dialect: "postgres",
 });
 
 try {
@@ -11,7 +14,6 @@ try {
   console.log("db connected");
 } catch (error) {
   console.error("db con error:", error);
-  console.error("Error details:", error.original);
 }
 
 export default db;
